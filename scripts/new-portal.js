958d730352814a05b7cfe4ad49ad346eb86de101
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-01 21:23:34 +0600

    sorting options section complate

--- scripts/new-portal.js
+++ scripts/new-portal.js
@@ -38,4 +38,8 @@ const showAllNews = (data , category_name) =>{
 
     document.getElementById("news-count").innerText = data.length;
     document.getElementById("category-name").innerText =  category_name;
+
+    data.forEach(singleNews => {
+        console.log(singleNews)
+    })
 };
