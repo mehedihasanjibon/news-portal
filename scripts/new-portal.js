80d1a8413cc011096df6b9e09ef86a16086133e3
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-01 22:45:50 +0600

    card all news in a category api conect

--- scripts/new-portal.js
+++ scripts/new-portal.js
@@ -34,7 +34,7 @@ const fetchCategoryNews = (category_id , category_name) =>{
 };
 
 const showAllNews = (data , category_name) =>{
-    console.log(data, category_name);
+    // console.log(data, category_name);
 
     document.getElementById("news-count").innerText = data.length;
     document.getElementById("category-name").innerText =  category_name;
@@ -45,23 +45,64 @@ const showAllNews = (data , category_name) =>{
 
     data.forEach(singleNews => {
         // console.log(singleNews);
+
+        const { _id,image_url, title, details, author, total_view} = singleNews;
+
         // newsContainer.innerHTML += ``;
         const card = document.createElement("div");
         card.classList.add("card", "mb-3");
         card.innerHTML = `
-        <div class="row g-0">
+        <div class="row g-0 p-3">
             <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
+            <img src=${image_url} class="img-fluid rounded-start" alt="...">
             </div>
-        <div class="col-md-8">
+
+        <div class="col-md-8 d-flex flex-column">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">
+                ${details.slice(0,200)}...
+            </p>
           </div>
+
+          <div class = "card-footer border-0 bg-body d-flex justify-content-between">
+            <div class="d-flex gap-2">
+            <img src=${author.img} class="img-fluid rounded-circle" alt="..." height="40" width="40"/>
+                <div>
+                <p class="m-0 p-0"> ${author.name} </p>
+                <p class="m-0 p-0"> ${author.published_date} </p>
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <i class="fas fa-eye"></i>
+                <p class="m-0 p-0"> ${total_view} </p>
+            </div>
+
+            <div>
+            <i class="fas fa-star"></i>
+            </div>
+
+            <div>
+            <i class="fas fa-arrow-right" onclick="fetchNewsDetail('${_id}')"></i>
+            </div>
+          </div>
+
             </div>
         </div>
         `;
         newsContainer.appendChild(card);
-    })
+    });
 };
+
+const fetchNewsDetail =  news_id => {
+    let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showNewsDetail(data.data[0]));
+};
+
+const showNewsDetail = newsDetail => {
+    
+}
